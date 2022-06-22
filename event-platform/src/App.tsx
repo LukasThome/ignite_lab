27bb74c09fc5e6bf7d8624d4fbe1818bb7f781e3
LukasThome <thome_lucas@live.com>
2022-06-21 23:00:58 -0300

    conceitos comentados

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -2,6 +2,8 @@ import { gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
 import { client } from "./lib/apollo"
 
+
+//query do graphQL
 const GET_LESSONS_QUERY = gql `
   query {
     lessons {
@@ -18,14 +20,14 @@ interface Lesson {
 }
 
 function App() {
-
+  //busca todas as lessons do graphCMS, utilizando a query do graphQL acima
   const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   //retorna o conteudo do array data
   console.log(data);
 
   return ( 
-    //retorna uma lista com os respectivos dados solicitados na query 
+    //retorna uma lista com os respectivos dados solicitados na query em tela
    <ul> 
     {data?.lessons.map(lesson  => {
       return <li key={lesson.id}>{lesson.title}</li>
